39755114f499e8f526536fdcefdbe65a94be2527
Pravas <pravasith@gmail.com>
2021-12-14 08:29:06 +0530

    chore: added tailwind and paths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
 
 const Home: NextPage = () => {
     return (
@@ -15,13 +15,13 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className={styles.main}>
+            <main className={`${styles.main} bg-red-400`}>
                 <h1 className={styles.title}>
                     Welcome to <a href="https://nextjs.org">Next.js!</a>
                 </h1>
 
                 <p className={styles.description}>
-                    Get started by editing{' '}
+                    Get started by editing{" "}
                     <code className={styles.code}>pages/index.tsx</code>
                 </p>
 
@@ -72,7 +72,7 @@ const Home: NextPage = () => {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Powered by{' '}
+                    Powered by{" "}
                     <span className={styles.logo}>
                         <Image
                             src="/vercel.svg"
@@ -84,7 +84,7 @@ const Home: NextPage = () => {
                 </a>
             </footer>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
